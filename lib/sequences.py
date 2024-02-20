06747af863ea7c6be6deb4eb7f2138409887b215
RumiYo <yokoyamarumi@gmail.com>
2024-02-19 23:54:19 -0500

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        result = []
+        print(result)
+    elif length == 1:
+        result = [0]
+        print(result)
+    elif length == 2:
+        result = [0, 1]
+        print(result)
+    else:
+        i = 3
+        result = [0, 1]
+        while i <= length :
+            result.append(result[i-3]+result[i-2])
+            i += 1
+        print(result)
+
+
